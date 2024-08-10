2e9e4451e62acaf1d24149de7799b99ab7a13559
yako0918 <yako0918124051@gmail.com>
2024-08-10 06:33:57 +0000

    08

--- button1.py
+++ button1.py
@@ -1,7 +1,12 @@
 import signal
-import gpiozero  import button
+from gpiozero import Button,LED
 
+def user_release():
+    print("使用者按下放開")
+    led.toggle() #狀態反轉
 
-if __name__ == '__name__':
-    Butyon
+if __name__ == '__main__': #主程式執行起點
+    button = Button(pin=18) # di 名稱
+    button.when_released = user_release 
+    led = LED(pin=25) # do 名稱
     signal.pause()
